b8211df24a36a91691c1340aa5a7ccd19aa046c6
teasacura <teasacuraster@gmail.com>
2018-05-08 17:29:22 -0400

    updated api_communicator.rb

--- api_communicator.rb
+++ api_communicator.rb
@@ -2,30 +2,36 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-class WeatherAPIGetter
+# class WeatherAPIGetter
   # url = api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
 
-  BASE_URL= "api.openweathermap.org/data/2.5/forecast?q="
+  # BASE_URL= "api.openweathermap.org/data/2.5/forecast?q="
 
   def get_weather_data
-    city_name = "Paris"
+    # puts "Which city would you like to view weather for? Please enter city."
+    # city_name = gets.chomp.downcase
+    # puts "Country code? Use us for United States"
+    # country_code = gets.chomp.downcase
+    # throw
+    city_name = "paris"
     country_code = "fr"
     apikey = "304bcd251ded0521b8be10e3c5743f37"
     weather_json = RestClient.get("api.openweathermap.org/data/2.5/forecast?q=#{city_name},#{country_code}&APPID=#{apikey}")
     puts weather_hash = JSON.parse(weather_json)
   end
 
-  def welcome_message
-    puts "Hello! Welcome to WeatherCLI"
-    puts "press h for help"
-    puts "Here's how to use this program:"
-    puts "press w to get weather"
-    puts "press q to quit"
-  end
+get_weather_data
 
-  def initialize
-    welcome_message
+  # def welcome_message
+  #   puts "Hello! Welcome to WeatherCLI"
+  #   puts "press w to get weather, press to q to exit"
+  # end
 
-  end
+  # def initialize
+  #   welcome_message
+  #   input = gets.chomp
+  #   if input == "w"
+  #     get_weather_data
+  # end
 
-end
+# end
