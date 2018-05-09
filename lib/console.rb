b4469ea619a6dcb22df3a5813b14c647d387bce7
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-05-09 14:29:05 -0400

    add set_link set_link_answer methods to console

--- lib/console.rb
+++ lib/console.rb
@@ -1,17 +1,36 @@
 class Interface
-  def set_question
+  def self.set_question
     puts "What's your question?"
     input = gets.strip
     Question.create(question: input)
   end
 
-  def set_answer(question)
+  def self.set_answer(question)
     puts "What's the answer?"
     input = gets.strip
     Answer.create(answer: input, question_id: question.id)
   end
 
-  def set_question_answer
+  def self.set_link(answer)
+    puts "What's the url?"
+    input_url = gets.strip
+    puts "And do you have a comment?"
+    input_comment = gets.strip
+    link = Link.create(url: input_url, comment: input_comment)
+    set_link_answer(link, answer)
+  end
+
+
+  # builder
+  def self.set_question_answer
     set_answer(set_question)
   end
+
+
+
+  # not for user, eventually private
+  def self.set_link_answer(link, answer)
+    LinkAnswer.create(link_id: link.id, answer_id: answer.id)
+  end
+
 end
