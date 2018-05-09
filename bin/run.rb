0fea03a611facb49276326f76fba99808e63316a
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-05-09 11:44:10 -0400

    commented out later  methods and deleted database bc rake db:drop wasnt working

--- bin/run.rb
+++ bin/run.rb
@@ -3,4 +3,4 @@
 require_relative 'config/environment'
 
 ###do i need to require 'lib', if it's already required in config/environment which is required above?
-require_all 'lib'
+# require_all 'lib'
