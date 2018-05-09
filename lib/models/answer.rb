35edbbea6920044d37b918c9a767251b01e786a4
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-05-09 12:25:38 -0400

    add many through relationships on Link and Answer

--- lib/models/answer.rb
+++ lib/models/answer.rb
@@ -1,4 +1,5 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
-  has_many :links 
+  has_many :link_answers
+  has_many :links, through: :link_answers
 end
--- lib/models/link.rb
+++ lib/models/link.rb
@@ -1,3 +1,4 @@
 class Link < ActiveRecord::Base
   has_many :link_answers
+  has_many :answers, through: :link_answers
 end
