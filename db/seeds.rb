3f7a06484e139a67b310037aedae2393cc15e0cf
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-05-09 21:12:01 -0400

    uncommented out dummy users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 require_relative './scraper.rb'
-require 'pry'
+# require 'pry'
 
 i = 0
 while i < 10
@@ -10,7 +10,7 @@ end
 User.create(name: "Saige", age: 22, home_state: "NY")
 User.create(name: "Jesse", age: 30, home_state: "CT")
 
-first = FourTwentyTrip.create(date_of_trip: "April 20th, 2019")
-second = FourTwentyTrip.create(date_of_trip: "April 20th, 2020")
-third = FourTwentyTrip.create(date_of_trip: "April 20th, 2021")
+# first = FourTwentyTrip.create(date_of_trip: "April 20th, 2019")
+# second = FourTwentyTrip.create(date_of_trip: "April 20th, 2020")
+# third = FourTwentyTrip.create(date_of_trip: "April 20th, 2021")
 
