dbeb068149b03b4805e23f80ef3f49c20f7d778d
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-05-10 16:38:15 -0400

    add run method to runner file, add logger silencer for sql queries in environment

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'lib'
+
+# remove this to see the sql queries in the return value
+old_logger = ActiveRecord::Base.logger
+ActiveRecord::Base.logger = nil
