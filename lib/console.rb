75e4de1a521af289727aed9578ea2606615fcf7c
snkoski <shawnnkoski@gmail.com>
2018-05-10 19:19:43 -0400

    move set_link_answer method to LinkAnswer class

--- lib/console.rb
+++ lib/console.rb
@@ -1,59 +1,63 @@
 class Interface
+#question
   def self.set_question
     puts "What's your question?"
     input = gets.strip
     Question.create(question: input)
   end
-
+#answer
   def self.set_answer(question)
     puts "What's the answer?"
     input = gets.strip
     Answer.create(answer: input, question_id: question.id)
   end
-
+#link
   def self.set_link(answer)
     puts "What's the url?"
     input_url = gets.strip
     puts "And do you have a comment?"
     input_comment = gets.strip
     link = Link.create(url: input_url, comment: input_comment)
-    set_link_answer(link, answer)
+    LinkAnswer.set_link_answer(link, answer)
   end
-
+#tag
   def self.create_new_tag(tag_name)
     Tag.create(t_content: tag_name)
   end
-
+#tag_answer
   def self.create_tag_answer(tag, answer)
     TagAnswer.create(tag: tag, answer: answer)
   end
 
 
   # builder
-  def self.set_question_answer
-    set_answer(set_question)
-  end
+#answer
+  # def self.set_question_answer
+  #   set_answer(set_question)
+  # end
 
   # get arrays of question and answer combos
+#question
   def self.get_all_questions
     return Question.all.map { |q| q }
   end
 
+#question
   def self.get_all_answered_questions
     return Question.all.select { |q| q.answer != nil }
   end
-
+#question
   def self.get_all_unanswered_questions
     return Question.all.select { |q| q.answer.nil? }
   end
-
+#question
   def self.list_questions(arr)
     arr.length.times { |i| puts "#{i+1}. #{arr[i].question}" }
   end
 
 
   # gets and prints out various combos of q's, a's, and links
-
+#tag
   def self.list_all_tags
     tags = Tag.all
     tags.each_with_index do |tag,i|
@@ -62,7 +66,7 @@ class Interface
     end
     puts ""
   end
-
+#question
   def self.list_questions_by_tag(tag)
     arr = self.get_all_answered_questions
     arr.each_with_index do |question, i|
@@ -73,7 +77,7 @@ class Interface
       end
     end
   end
-
+#tag
   def self.list_question_tags(question)
     puts "TAGS: "
     question.answer.tags.each {|tag| print tag.t_content + ' | '}
@@ -81,6 +85,7 @@ class Interface
   end
 
   # THIS ONE
+#question
   def self.all_questions_with_answers
     arr = self.get_all_answered_questions
     arr.each_with_index do |question, i|
@@ -91,13 +96,13 @@ class Interface
       # self.list_question_tags(question)
     end
   end
-
+#link
   def self.list_all_links
     puts "Download iTerm2(TM) to visit links from command line by holding cmd when you click."
     links = Link.all
     links.length.times { |i| puts "#{i+1}. #{links[i].url}\n#{links[i].comment}" }
   end
-
+#link
   def self.print_links(answer)
     answer.links.each do |link|
       puts "* #{link.url}\n\t#{link.comment}"
@@ -105,6 +110,7 @@ class Interface
   end
 
   # THIS ONE
+#question
   def self.list_everything
     self.get_all_answered_questions.each_with_index do |q, i|
       puts  "-" * 20
@@ -117,19 +123,19 @@ class Interface
   end
 
   # update methods
-
+#question
   def self.update_question(question)
     puts "What's your edited question?"
     input = gets.strip
     question.update(question: input)
   end
-
+#answer
   def self.update_answer(answer)
     puts "What's your edited answer?"
     input = gets.strip
     answer.update(answer: input)
   end
-
+#link
   def self.update_link(link)
     puts "What's your edited link url?"
     new_url = gets.strip
@@ -140,7 +146,7 @@ class Interface
 
 
   # selector methods
-
+#question
   def self.question_object_from_menu(index_from_menu)
     # BE SURE TO SHOW USER THE GET_ALL_QUESTIONS ARRAY FOR THE DELETE MENU
     arr = self.get_all_questions
@@ -148,12 +154,12 @@ class Interface
   end
 
     # delete methods
-
+#question
   def self.destroy_links(index_from_menu)
     question = self.question_object_from_menu(index_from_menu)
     self.destroy_question(question) if question
   end
-
+#question??????
   def self.destroy_answer_links(index_from_menu)
     question = self.question_object_from_menu(index_from_menu)
     if question
@@ -161,7 +167,7 @@ class Interface
       self.destroy_answer(question)
     end
   end
-
+#question
   def self.destroy_question_answer_links_tags(index_from_menu)
     question = self.question_object_from_menu(index_from_menu)
     if question
@@ -171,33 +177,33 @@ class Interface
       self.destroy_question(question)
     end
   end
-
+#question
   def self.destroy_question(question)
     question.destroy if question
   end
-
+#question
   def self.destroy_answer(question)
     question.answer.destroy if question.answer
   end
-
+#question
   def self.destroy_links(question)
     question.answer.links.each { |link| link.destroy } if question.answer
   end
-
+#tag_answer
   def self.destroy_tag_from_answer(tag,answer)
     if answer
       tag_answer = TagAnswer.where("tag_id = ? AND answer_id = ?", tag[:id], answer[:id])[0]
       tag_answer.destroy
     end
   end
-
+#question
   def self.destroy_all_tags_from_question(question)
     if question.answer
       tags = question.answer.tags
       tags.each { |tag| self.destroy_tag_from_answer(tag,question.answer) }
     end
   end
-
+#tag
   def self.destroy_tag_from_all(tag)
     if tag
       TagAnswer.where("tag_id = ?", tag[:id]).destroy_all
@@ -206,10 +212,11 @@ class Interface
   end
 
 
-  # not for user, eventually private
-  def self.set_link_answer(link, answer)
-    LinkAnswer.create(link_id: link.id, answer_id: answer.id)
-  end
+#   # not for user, eventually private
+# #link_answer
+#   def self.set_link_answer(link, answer)
+#     LinkAnswer.create(link_id: link.id, answer_id: answer.id)
+#   end
 
 end
 
--- lib/models/link_answer.rb
+++ lib/models/link_answer.rb
@@ -1,4 +1,11 @@
 class LinkAnswer < ActiveRecord::Base
   belongs_to :link
   belongs_to :answer
+
+  # not for user, eventually private
+  #link_answer
+  def self.set_link_answer(link, answer)
+    LinkAnswer.create(link_id: link.id, answer_id: answer.id)
+  end
+
 end
